eaef676329def0b01ebcbb1b0a2115c570abec8b
HackwareBro <hackwarebro@gmail.com>
2021-08-11 20:57:48 +0500

    get_webpage method has been added
    get_webpage() takes url and it's data and simply returns the html structured bs4 object to play with it

--- job-finder.py
+++ job-finder.py
@@ -1,5 +1,5 @@
 import requests,os
-
+from bs4 import BeautifulSoup
 #Global vars
 
 def take_user_data():
@@ -19,9 +19,7 @@ def take_user_data():
     
     return {'jobs':data[0],'location':data[1],'exp':data[2]}
 
-if __name__=='__main__':
-    data = take_user_data()
-    #Create request packet 
+def get_webpage(web_url, data): #data var to send in params of link
     input_values = {
         'searchType':'personalizedSearch',
         'from':'submit',
@@ -29,4 +27,10 @@ if __name__=='__main__':
         'txtLocation': data['location'],
         "cboWorkExp1": data['exp']
     }
-    response = requests.get('https://www.timesjobs.com/candidate/job-search.html',params=input_values)
+    response = requests.get(web_url,params=input_values)
+    return BeautifulSoup(response.text,'lxml')
+
+if __name__=='__main__':
+    data = take_user_data()
+    #Create request packet 
+    html = get_webpage('https://www.timesjobs.com/candidate/job-search.html',data)
