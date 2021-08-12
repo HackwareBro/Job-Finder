04d34f82e52adef30547dbe56645c38cebfc61b8
HackwareBro <hackwarebro@gmail.com>
2021-08-12 16:23:21 +0500

    extract_data() method has been fully implemented.
    Some bugs are needed to be removed.

--- job-finder.py
+++ job-finder.py
@@ -32,7 +32,50 @@ def get_webpage(web_url, data): #data var to send in params of link
     response = requests.get(web_url,params=input_values)
     return BeautifulSoup(response.text,'lxml')
 
+def extract_data(html_page):
+    jobs = html_page.find_all('li',class_='clearfix job-bx wht-shd-bx')
+    #data to be extract
+    complete_data = []
+    single_data = (        
+        'job',
+        'company' ,
+        'location',
+        'skills',
+        'link',
+    )
+    single_data = dict.fromkeys(single_data,'') #to create dict having empty string values
+    for job in jobs:
+        posted_day = job.find('span',class_='sim-posted').text
+        if 'few' in posted_day:
+            h2 = job.find('h2')
+            single_data['job'] = h2.a.text.strip('\n \r')
+            single_data['link'] = h2.a['href'].strip('\n \r')
+            single_data['company'] = job.find('h3').text.strip('\n \r')   
+            single_data['location'] = job.find('ul', class_='top-jd-dtl clearfix').span.text.strip('\n \r')
+            single_data['skills'] = job.find('span',class_='srp-skills').text.strip('\n \r')
+            complete_data.append(single_data)
+            single_data = dict.fromkeys(single_data,'') # to reset the data
+    return complete_data  
+
 if __name__=='__main__':
+    print("""   ___       _      ______ _           _
+  |_  |     | |     |  ___(_)         | |
+    | | ___ | |__   | |_   _ _ __   __| | ___ _ __
+    | |/ _ \| '_ \  |  _| | | '_ \ / _` |/ _ \ '__|
+/\__/ / (_) | |_) | | |   | | | | | (_| |  __/ |
+\____/ \___/|_.__/  \_|   |_|_| |_|\__,_|\___|_|
+
+Made By: Hackware Bro    
+    """)
     data = take_user_data() 
     #Create request packet 
     html = get_webpage('https://www.timesjobs.com/candidate/job-search.html',data)
+    complete_data = extract_data(html)
+    for entry in complete_data:
+        print(f"""
+Job Name : {entry['job']}
+Company name : {entry['company']}
+Key-Skills Required : {entry['skills']}
+Location : {entry['location']}
+Visit the website for more info : {entry['link']}
+        """)
